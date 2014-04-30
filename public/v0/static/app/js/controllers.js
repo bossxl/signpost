6692f9a245664b4d38f3da8dd4c5ebac5f2ae9a3
Paul Boss <paul.boss@rakutenloyalty.com>
2014-04-30 15:34:26 -0400

    Add new pages

--- public/v0/static/app/js/controllers.js
+++ public/v0/static/app/js/controllers.js
@@ -8,4 +8,22 @@ angular.module('myApp.controllers', [])
   }])
   .controller('MyCtrl2', [function() {
 
+  }])
+  .controller('Finder', ['$scope', '$http', function($scope, $http) {
+  	$scope.reset = function(){
+  		$scope.coor = {};
+  	}
+    $scope.submit = function(coor){
+      var url = "http://localhost/v0/points?lat=%lat&long=%long";
+      url = url.replace("%lat", coor.lat);
+      url = url.replace("%long", coor.long);
+      $http.get(url).success(function($data){
+        $scope.locations = $data;
+      }).error(function(){
+        $scope.error = "Finder failed. Sorry";
+      })
+    }
+  }])
+  .controller('Digger', [function() {
+
   }]);
